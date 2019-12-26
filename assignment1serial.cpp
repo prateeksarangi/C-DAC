7d7b395e7f5b1116f524b70dc16b5922d8bf49db
Prateek Sarangi <sarangiprateek80@gmail.com>
2019-12-26 19:29:36 +0530

    Add files via upload

--- assignment1serial.cpp
+++ assignment1serial.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<stdio.h>
-#include<omp.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -17,7 +15,6 @@ int main(int argc, char const *argv[])
 
     for(i=0; i<m; i++)
         for(j=0; j<m; j++)
-            #pragma omp parallel for
             for(k=0; k<m; k++)
                 printf("Executing in thread number:- %d\nResult:- %c%c%c\n", omp_get_thread_num(), ch[i], ch[j], ch[k]);
                 

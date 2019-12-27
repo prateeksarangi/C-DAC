146f6c8532ff6459789900cbfeea116699dbebbc
Prateek Sarangi <sarangiprateek80@gmail.com>
2019-12-27 10:46:55 +0530

    Add files via upload

--- assignment1parallel.cpp
+++ assignment1parallel.cpp
@@ -15,11 +15,15 @@ int main(int argc, char const *argv[])
         t++;
     }
 
+    double before = omp_get_wtime();
+    #pragma omp parallel
     for(i=0; i<m; i++)
         for(j=0; j<m; j++)
-            #pragma omp parallel for
+            #pragma omp for
             for(k=0; k<m; k++)
                 printf("Executing in thread number:- %d\nResult:- %c%c%c\n", omp_get_thread_num(), ch[i], ch[j], ch[k]);
                 
+    double after = omp_get_wtime();
+    cout<<"Time total:- "<<after - before<<endl;
     return 0;
 }
--- assignment1serial.cpp
+++ assignment1serial.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<omp.h>
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -13,10 +14,13 @@ int main(int argc, char const *argv[])
         t++;
     }
 
+    double before = omp_get_wtime();
     for(i=0; i<m; i++)
         for(j=0; j<m; j++)
             for(k=0; k<m; k++)
                 printf("Executing in thread number:- %d\nResult:- %c%c%c\n", omp_get_thread_num(), ch[i], ch[j], ch[k]);
-                
+              
+    double after = omp_get_wtime();
+    cout<<"Time total:- "<<after - before<<endl;  
     return 0;
 }

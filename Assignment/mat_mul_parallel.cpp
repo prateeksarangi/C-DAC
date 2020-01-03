ef7255661c6cd280abe9a0190d6b6e838c61b90e
prateeksarangi <sarangiprateek80@gmail.com>
2020-01-03 15:11:38 +0530

    Parallel program of matrix multiplication added

--- Assignment/mat_mul_parallel.cpp
+++ Assignment/mat_mul_parallel.cpp
@@ -2,45 +2,90 @@
 WAP to multipy two matrix.(Using MPI)
 */
 
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+#include "mpi.h"
+#define N 4
 
-int main(){
-	int n, m, p, q;
-	int i, j, k;
-	
-	cout<<"Enter the dimension of the first matrix:-\n";
-	cin>>m>>n;
+MPI_Status status;
 
-	cout<<"Enter the dimension of the second matrix:-\n";
-	cin>>p>>q;
+double a[N][N],b[N][N],c[N][N];
 
-	if(n == p){
-		int a[m][n], b[p][q], c[m][q];
-		cout<<"Enter the elements of the first matrix:-\n";
-		for(i=0; i<m; i++)
-			for(j=0; j<n; j++)
-				cin>>a[i][j];
+main(int argc, char **argv)
+{
+  int numtasks,taskid,numworkers,source,dest,rows,offset,i,j,k;
 
-		cout<<"Enter the elements of the second matrix:-\n";
-		for(i=0; i<p; i++)
-			for(j=0; j<q; j++)
-				cin>>b[i][j];
+  struct timeval start, stop;
 
-		for(i=0; i<m; i++)
-			for(j=0; j<q; j++)
-			{
-				c[i][j]=0;
-				for(k=0; k<n; k++)
-					c[i][j] += a[i][k]*b[k][j];
-			}
-		cout<<"The result matrix is:-\n";
-		for(i=0; i<m; i++){
-			for(j=0; j<q; j++)
-				cout<<c[i][j]<<" ";
-			cout<<endl;
-		}
-	}
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-	cout<<endl;
+  numworkers = numtasks-1;
+
+  if (taskid == 0) {
+    for (i=0; i<N; i++) {
+      for (j=0; j<N; j++) {
+        a[i][j]= 1.0;
+        b[i][j]= 2.0;
+      }
+    }
+
+    gettimeofday(&start, 0);
+
+    rows = N/numworkers;
+    offset = 0;
+
+    for (dest=1; dest<=numworkers; dest++)
+    {
+      MPI_Send(&offset, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&rows, 1, MPI_INT, dest, 1, MPI_COMM_WORLD);
+      MPI_Send(&a[offset][0], rows*N, MPI_DOUBLE,dest,1, MPI_COMM_WORLD);
+      MPI_Send(&b, N*N, MPI_DOUBLE, dest, 1, MPI_COMM_WORLD);
+      offset = offset + rows;
+    }
+
+    for (i=1; i<=numworkers; i++)
+    {
+      source = i;
+      MPI_Recv(&offset, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&rows, 1, MPI_INT, source, 2, MPI_COMM_WORLD, &status);
+      MPI_Recv(&c[offset][0], rows*N, MPI_DOUBLE, source, 2, MPI_COMM_WORLD, &status);
+    }
+
+    gettimeofday(&stop, 0);
+
+    printf("Here is the result matrix:\n");
+    for (i=0; i<N; i++) {
+      for (j=0; j<N; j++)
+        printf("%6.2f   ", c[i][j]);
+      printf ("\n");
+    }
+
+    fprintf(stdout,"Time = %.6f\n\n",
+         (stop.tv_sec+stop.tv_usec*1e-6)-(start.tv_sec+start.tv_usec*1e-6));
+
+  }
+
+  if (taskid > 0) {
+    source = 0;
+    MPI_Recv(&offset, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&rows, 1, MPI_INT, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&a, rows*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+    MPI_Recv(&b, N*N, MPI_DOUBLE, source, 1, MPI_COMM_WORLD, &status);
+
+    /* Matrix multiplication */
+    for (k=0; k<N; k++)
+      for (i=0; i<rows; i++) {
+        c[i][k] = 0.0;
+        for (j=0; j<N; j++)
+          c[i][k] = c[i][k] + a[i][j] * b[j][k];
+      }
+
+
+    MPI_Send(&offset, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
+    MPI_Send(&c, rows*N, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
 }
